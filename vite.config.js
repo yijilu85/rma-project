d1e25d3d33dbfe5e05ee6eccaddf766d1cffcf75
s87410 <yiji85@googlemail.com>
2024-01-22 01:08:32 +0100

    vite config

--- vite.config.js
+++ vite.config.js
@@ -7,10 +7,10 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/rma-project/',
-  plugins: [vue(), vueJsx()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+  plugins: [vue(), vueJsx()]
+  // resolve: {
+  //   alias: {
+  //     '@': fileURLToPath(new URL('./src', import.meta.url))
+  //   }
+  // }
 })
